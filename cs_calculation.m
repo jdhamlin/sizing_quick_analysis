33750e790d598e45de2bad13a330539b2162c651
jdhamlin <jdhamlin@ucsd.edu>
2026-03-20 11:57:24 -0700

    Update cs_calculation.m
    Corrected calculation to prevent issues with unit conversion and to convert normalized concentration measurements to integral concentrations across each size bin.

--- cs_calculation.m
+++ cs_calculation.m
@@ -16,36 +16,41 @@
 function [CS] = cs_calculation(D, dN)
 
 % constants 
-lambda = 0.66e-6; % m; gas mean free path
-D_SA = 0.8e-5; % diffusion coefficient of H2SO4
+lambda = 0.66e-6; % m, gas mean free path
+D_SA = 0.8e-5; % m^2/s, diffusion coefficient of H2SO4
 
 %%% unit conversions
-% if units are in m, convert to nm
-if max(D) < 1e-6
+maxD = max(D);
+
+if maxD < 1e-6 % if units are in m, convert to nm
     D = D * 1e9;
 
-    % if units in um, convert to nm
-elseif max(D) < 1
+elseif maxD < 1e-3 % if units in um, convert to nm
     D = D * 1e3;
+
 end
 
 % D from nm to m
-Dp = D * (10^-9);
+Dp = D * 1e-9;
+
+% determine dlogDp
+logDp = log10(Dp);
+dlogDp = diff(logDp);
+dlogDp = [dlogDp(1), dlogDp];
 
 % dN from cm^-3 to m^-3
-dN = dN * 10^6;
+dN = dN * 1e6;
 
 % calculate size dependent variables
 
 % Knudsen Number
-Kn = 2*lambda ./ Dp; % unitless; 
-Beta_m_numerator = 1 + Kn;
-Beta_m_denominator = 1 + (1.677 * Kn) + (1.33 * Kn.^2);
-Beta_m = Beta_m_numerator ./ Beta_m_denominator; % unitless; 
+Kn = 2*lambda ./ Dp; % unitless 
+Beta_m = (1 + Kn) ./ (1 + 1.677 * Kn + 1.33 * Kn.^2); % unitless
 
 % calculate condensation sink
+CS = zeros(size(dN, 1), 1);
 for i = 1:size(dN, 1)
-    CS(i,:) = 2*pi*D_SA*sum(Beta_m .* Dp .* dN(i,:));
+    CS(i,1) = 4*pi*D_SA*sum(Beta_m .* Dp .* dN(i,:) .* dlogDp);
 end
 
 return
